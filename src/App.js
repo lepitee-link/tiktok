91c0fa251e3a2936e9009e9fc9a6bceebce26886
lepitee-link <biavivgaming2@gmail.com>
2025-02-05 11:25:18 +0700

    button install

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Container, Button } from "@mui/material";
+import { Container, Snackbar, Button, Alert } from "@mui/material";
 import BarangList from "./components/BarangList";
 
 const App = () => {
   const [barangData, setBarangData] = useState([]);
   const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/data/dataBarang.json");
+        const response = await fetch("/tiktok/data/dataBarang.json");
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
@@ -27,10 +28,9 @@ const App = () => {
     const handler = (event) => {
       event.preventDefault();
       setDeferredPrompt(event);
-      console.log("Install prompt event saved.");
+      setOpenSnackbar(true);
     };
     window.addEventListener("beforeinstallprompt", handler);
-
     return () => window.removeEventListener("beforeinstallprompt", handler);
   }, []);
 
@@ -44,6 +44,7 @@ const App = () => {
           console.log("User dismissed the install prompt.");
         }
         setDeferredPrompt(null);
+        setOpenSnackbar(false);
       });
     }
   };
@@ -51,15 +52,27 @@ const App = () => {
   return (
     <Container style={{ maxWidth: "475px", paddingLeft: 0, paddingRight: 0 }}>
       <BarangList barangData={barangData} />
-      {deferredPrompt && (
-        <Button
-          onClick={handleInstallClick}
-          variant="contained"
-          color="primary"
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={() => setOpenSnackbar(false)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert
+          onClose={() => setOpenSnackbar(false)}
+          severity="info"
+          sx={{ backgroundColor: "#FFA500", color: "white" }}
         >
-          Install App
-        </Button>
-      )}
+          Pasang aplikasi untuk pengalaman lebih baik!
+          <Button
+            onClick={handleInstallClick}
+            size="small"
+            sx={{ marginLeft: 1, color: "white", fontWeight: "bold" }}
+          >
+            Pasang
+          </Button>
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
